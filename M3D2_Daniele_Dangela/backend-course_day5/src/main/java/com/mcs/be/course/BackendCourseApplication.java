6aea8cf6e751be95ba263e289b5ca54297ebc018
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-28 15:05:31 +0200

    aggiornamento

--- M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/BackendCourseApplication.java
+++ M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/BackendCourseApplication.java
@@ -2,7 +2,6 @@ package com.mcs.be.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
--- M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/facade/impl/UserFacadeImpl.java
+++ M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/facade/impl/UserFacadeImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.mcs.be.course.dao.CustomerDao;
 import com.mcs.be.course.facade.UserFacade;
@@ -11,6 +12,7 @@ import com.mcs.be.course.model.Customer;
 
 import ma.glasnost.orika.MapperFacade;
 
+@Component
 public class UserFacadeImpl implements UserFacade{
 	
 	
