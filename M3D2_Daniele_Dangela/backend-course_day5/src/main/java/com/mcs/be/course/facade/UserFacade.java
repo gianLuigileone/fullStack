ccb667d981bd825beed9b6a67d07fcfcd1c45278
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-28 12:36:20 +0200

    aggiornamento

--- M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/facade/UserFacade.java
+++ M3D2_Daniele_Dangela/backend-course_day5/src/main/java/com/mcs/be/course/facade/UserFacade.java
@@ -1,6 +1,8 @@
 package com.mcs.be.course.facade;
 
+import com.mcs.be.course.model.Customer;
+
 public interface UserFacade {
-	public User login(String id, String password);
-	public User register(User user);
+	public Customer login(String id, String password);
+	public Customer register(Customer customer);
 }
